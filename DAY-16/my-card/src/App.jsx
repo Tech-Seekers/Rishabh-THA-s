2f533483323ed7b96aa51ac9614e77080c304ba0
Rishabh0077 <53342690+Rishabh0077@users.noreply.github.com>
2021-07-12 17:32:01 +0530

    Add files via upload

---
+++ DAY-16/my-card/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/Card";
+import "./style.css";
+
+function App(){
+    return(
+        <div className="App">
+            <Card />
+            <Card />
+        </div>
+    );
+}
+
+export default App; 
